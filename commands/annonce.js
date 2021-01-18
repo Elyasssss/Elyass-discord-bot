b39d185802453285b272323778d0a738b7eb5534
Elyasssss <77575328+Elyasssss@users.noreply.github.com>
2021-01-18 17:57:49 +0100

    Update annonce.js

--- commands/annonce.js
+++ commands/annonce.js
@@ -8,10 +8,13 @@ module.exports = {
 //Dans l'embed.
 const exampleEmbed = new Discord.MessageEmbed()
 	.setColor('#0099ff')
-	.setTitle('Annonce')
-    .setDescription('Par exemple keyzen ,\n Je suis une annonce type^^')
-    .setTimestamp()
-    .setFooter('Amusez-vous bien^^', 'https://cdn.discordapp.com/avatars/559081819920662538/08d5db049d31dd414f6315de81814f5e.webp');
+	.setTitle('Annonce sans @')
+	.setAuthor('Elyass Bot', 'https://cdn.discordapp.com/avatars/736981928518877267/b52c6cdaf6454854e6d3e50ded050dc9.png')
+	.setDescription('Je tenais à vous annoncez que je vais être en maintenance pendant un petit moment.\n Mon Créateur à décider de me modifier un peu , rajouter des choses ect... \n Je reviendrai Meilleur et serai là 24h/24.')
+	.setThumbnail('https://cdn.discordapp.com/avatars/559081819920662538/08d5db049d31dd414f6315de81814f5e.webp')
+	.setImage('https://cdn.discordapp.com/attachments/760830394555170847/800769586412257280/unknown.png')
+	.setTimestamp()
+        .setFooter('Amusez-vous bien^^', 'https://cdn.discordapp.com/avatars/559081819920662538/08d5db049d31dd414f6315de81814f5e.webp');
 
     message.channel.send(exampleEmbed);
     
