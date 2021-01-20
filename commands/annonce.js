165e560ee74a1bab8cd89a28755c53959bbf7dff
Elyasssss <77575328+Elyasssss@users.noreply.github.com>
2021-01-20 13:34:58 +0100

    Update annonce.js

--- commands/annonce.js
+++ commands/annonce.js
@@ -10,16 +10,15 @@ const exampleEmbed = new Discord.MessageEmbed()
 	.setColor('#0099ff')
 	.setTitle('Annonce sans @')
 	.setAuthor('Elyass Bot', 'https://cdn.discordapp.com/avatars/736981928518877267/b52c6cdaf6454854e6d3e50ded050dc9.png')
-	.setDescription('·Je tenais à vous prévenir que je serais en maintenance pendant un petit moment ! \n ·Mon créateur à décidé de me modifier un peu afin de rajouter quelques améliorations ect...')
+	.setDescription('·Je reviens en force !\n ·Je suis actuellement en ligne h24 ! Les commandes pourront donc ce faire à tout moment de la journée^^ \n Profitez bien ! ')
         .setThumbnail('https://cdn.discordapp.com/avatars/559081819920662538/08d5db049d31dd414f6315de81814f5e.webp')
 	.addFields(
-		{ name: 'Voici quelques avant goûts', value: 'Vous êtes prêts ?' },
+		{ name: 'je reviens donc', value: 'Avec pleins de nouveautés !' },
 		{ name: '\u200B', value: '\u200B' },
-		{ name: 'Un bot 24h/24 !', value: 'Et 7j/7 !', inline: true },
-		{ name: 'Des commandes supplémentaires !', value: 'Mais avec des embeds !', inline: true },
+		{ name: 'Je serai donc un bot 24h/24 !', value: 'Et 7j/7 !', inline: true },
 	)
 	.addField('Et je vous souhaite donc...', 'Une bonne soirée à tous ! :D', true)
-	.setImage('https://cdn.discordapp.com/attachments/760830394555170847/800769586412257280/unknown.png')
+	.setImage('https://cdn.discordapp.com/attachments/798882442336534588/801429145178996776/unknown.png')
 	.setTimestamp()
 	.setFooter('Amusez-vous bien^^', 'https://cdn.discordapp.com/avatars/559081819920662538/08d5db049d31dd414f6315de81814f5e.webp');
     
